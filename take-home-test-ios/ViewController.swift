bf01e82edf0a1efc768d03bc46e240ad1f0755db
amirmorsypaysky <amir.morsy@paysky.io>
2025-01-02 14:57:28 +0200

    Initial Commit

---
+++ take-home-test-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  take-home-test-ios
+//
+//  Created by PaySky106 on 02/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ take-home-test-iosTests/take_home_test_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  take_home_test_iosTests.swift
+//  take-home-test-iosTests
+//
+//  Created by PaySky106 on 02/01/2025.
+//
+
+import Testing
+@testable import take_home_test_ios
+
+struct take_home_test_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
